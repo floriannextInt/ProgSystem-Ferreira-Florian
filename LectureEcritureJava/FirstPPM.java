2c3ff9d375ecad76435f447bcdd58babd782f7f0
floriannextInt <florian.ferreira@iut-rodez.fr>
2026-09-15 22:29:31 +0200

    creation tp1

---
+++ LectureEcritureJava/FirstPPM.java
@@ -0,0 +1,27 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FirstPPM {
+    public static void main(String[] args) {
+        try {
+            FileWriter writer = new FileWriter("FirstPPM.ppm");
+
+            writer.write("P3\n");
+            // Écriture des dimensions
+            writer.write("3 2\n");
+            // Écriture de la valeur maximal
+            writer.write("255\n");
+            // Écriture des pixels
+            // Première ligne : rouge, vert, bleu
+            writer.write("255 0 0 0 255 0 0 0 255\n");
+            // Deuxième ligne : jaune, blanc, noir
+            writer.write("255 0 0 0 255 0 0 0 255\n");
+
+            writer.close(); // Fermeture du fichier
+
+            System.out.println("Image PPM créée avec succès !");
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
+        }
+    }
+}
